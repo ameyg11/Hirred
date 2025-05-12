686fc2d8c5d81b8c1699e96b4087717370d40de2
Amey <ameygawade11@gmail.com>
2025-05-12 22:53:20 +0530

    added a grid background , made a app in dark mode using theme provider , created header file with navbar logo & login

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,11 +1,15 @@
-
+import Header from '@/components/header'
 import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
   return (
     <div>
-      <h1>App Layout</h1>
-      <Outlet />
+      <div className='grid-background'></div>
+      <main className='min-h-screen container'>
+        <Header />
+        <Outlet />
+      </main>
+      <div className='p-10 text-center bg-gray-800 mt-10'>Made by amey</div>
     </div>
   )
 }
