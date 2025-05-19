ef20926c2ac1e62111b10acc9d9bcbe87ff650f7
Amey <ameygawade11@gmail.com>
2025-05-19 20:29:35 +0530

    solved the freaking error

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -3,15 +3,16 @@ import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
   return (
-    <div>
-      <div className='grid-background '></div>
-        <main className='min-h-screen container '>
-          <Header />
-          <Outlet />
-        </main>
-      <div className='p-10 text-center bg-gray-800 mt-10'>Made by amey</div>
+    <div className="overflow-x-hidden">
+      <div className="grid-background"></div>
+      <main className="min-h-screen container mx-auto px-4">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">Made by amey</div>
     </div>
-  )
-}
+  );
+};
+
 
 export default AppLayout
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -4,7 +4,7 @@ const LandingPage = () => {
       <section className="text-center">
         <h1 className="flex flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">
           Find Your Dream Job{" "}
-          <span className="flex items-center gap-2 sm:gap-6">
+          <span className="flex items-center gap-2 sm:gap">
             and get{" "}
             <img
               src="/logo.png"
