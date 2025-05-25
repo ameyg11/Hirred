828bf0abb8aa792c5242b54dc9afdf171e1e999a
Amey <ameygawade11@gmail.com>
2025-05-25 19:50:29 +0530

    added some protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import PostJob from './pages/post-job'
 import SavedJobs from './pages/saved-job'
 import MyJobs from './pages/my-jobs'
 import { ThemeProvider } from "@/components/theme-provider"
+import ProtectedRoute from './components/protected-routes'
 
 const router = createBrowserRouter([
   {
@@ -22,27 +23,45 @@ const router = createBrowserRouter([
       },
       {
         path:'/onboarding',
-        element:<Onboarding />
+        element:
+        <ProtectedRoute >
+          <Onboarding />
+        </ProtectedRoute>
       },
       {
         path:'/jobs',
-        element:<JobListing />
+        element:
+        <ProtectedRoute>
+          <JobListing />
+        </ProtectedRoute>
       },
       {
         path:'/job/:id',
-        element:<JobPage />
+        element:
+        <ProtectedRoute >
+          <JobPage />
+        </ProtectedRoute>
       },
       {
         path:'/post-job',
-        element:<PostJob />
+        element:
+        <ProtectedRoute>
+          <PostJob />
+        </ProtectedRoute>
       },
       {
         path:'/saved-jobs',
-        element:<SavedJobs />
+        element:
+        <ProtectedRoute>
+          <SavedJobs />
+        </ProtectedRoute>
       },
       {
         path:'/my-jobs',
-        element:<MyJobs />
+        element:
+        <ProtectedRoute>
+          <MyJobs />
+        </ProtectedRoute>
       }
     ]
   }
--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,5 +1,11 @@
+import { useUser } from "@clerk/clerk-react"
 
 export default function Onboarding() {
+
+
+  const { user } = useUser();
+  console.log(user);
+
   return (
     <div>
       on Boarding
